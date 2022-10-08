0af1a7f78892ad91152cbfb21e847ba0ebbfa86e
Jay Tin <jaytintran@gmail.com>
2022-10-09 00:41:12 +0200

    first commit

---
+++ es6+.js
@@ -0,0 +1,16 @@
+// Template Literals
+let word1 = "Dylan"
+let word2 = "Kim"
+
+const fullName = `${word1} ${word2}`
+
+// Destructuring Objects
+const player = {
+    name: "Conor",
+    team: "Blue",
+    address: {
+        city: "LA"
+    }
+}
+
+let { name, team, address } = 
